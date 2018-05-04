58f53fcf48c19597472b0ec677c88d6bed07b626
Nataly <ncgarzon88@gmail.com>
2018-05-04 02:05:19 -0400

    updated section 3 and some footer work

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,18 @@
 console.log(`Hello`)
 
 
-var dropdown = document.querySelector("header nav ul");
-var burger = document.querySelector(".hamburger");
-var x = document.querySelector("nav ul div")
+// var dropdown = document.querySelector("header nav ul");
+// var burger = document.querySelector(".hamburger");
+// var x = document.querySelector("nav ul div")
 
-dropdown.style.display ="none";
+// dropdown.style.display ="none";
 
-burger.addEventListener("click", function(){
-	if( dropdown.style.display === "none" ) {
-		dropdown.style.display = "block";
-	}	
-});
+// burger.addEventListener("click", function(){
+// 	if( dropdown.style.display === "none" ) {
+// 		dropdown.style.display = "block";
+// 	}	
+// });
 
-x.addEventListener("click", function(){
-	dropdown.style.display = "none"
-});
+// x.addEventListener("click", function(){
+// 	dropdown.style.display = "none"
+// });
